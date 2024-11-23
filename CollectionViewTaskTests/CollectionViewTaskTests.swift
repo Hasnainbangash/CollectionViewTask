fbc34436c8ccad217ccba0dbeab883a95521911d
Bangash38 <bangashmhasnain38@gmail.com>
2024-11-23 21:10:04 +0500

    Initial Commit

---
+++ CollectionViewTaskTests/CollectionViewTaskTests.swift
@@ -0,0 +1,17 @@
+//
+//  CollectionViewTaskTests.swift
+//  CollectionViewTaskTests
+//
+//  Created by Muhammad Hasnain Bangash on 23/11/2024.
+//
+
+import Testing
+@testable import CollectionViewTask
+
+struct CollectionViewTaskTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
