56efb003b5a361ff7cf183b3eac43c7a7c5227c8
Bangash38 <bangashmhasnain38@gmail.com>
2024-11-23 22:39:09 +0500

    All functions working properly

---
+++ CollectionViewTask/Controller/ViewController.swift
@@ -0,0 +1,165 @@
+//
+//  ViewController.swift
+//  CollectionViewTask
+//
+//  Created by Muhammad Hasnain Bangash on 23/11/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    var images = [
+        UIImage(named: "picture1"),
+        UIImage(named: "picture2"),
+        UIImage(named: "picture3"),
+        UIImage(named: "picture4"),
+        UIImage(named: "picture5"),
+        UIImage(named: "picture1"),
+        UIImage(named: "picture2"),
+        UIImage(named: "picture3"),
+        UIImage(named: "picture4"),
+        UIImage(named: "picture5"),
+        UIImage(named: "picture1"),
+        UIImage(named: "picture1"),
+        UIImage(named: "picture2"),
+        UIImage(named: "picture3"),
+        UIImage(named: "picture4"),
+        UIImage(named: "picture5"),
+        UIImage(named: "picture1"),
+        UIImage(named: "picture2"),
+        UIImage(named: "picture3"),
+        UIImage(named: "picture4"),
+        UIImage(named: "picture5"),
+        UIImage(named: "picture1"),
+        UIImage(named: "picture5"),
+        UIImage(named: "picture1"),
+        UIImage(named: "picture2"),
+        UIImage(named: "picture3"),
+        UIImage(named: "picture4"),
+        UIImage(named: "picture5"),
+        UIImage(named: "picture1"),
+        UIImage(named: "picture1"),
+        UIImage(named: "picture3"),
+        UIImage(named: "picture4"),
+        UIImage(named: "picture5"),
+        UIImage(named: "picture1"),
+        UIImage(named: "picture1"),
+        UIImage(named: "picture2"),
+        UIImage(named: "picture3"),
+        UIImage(named: "picture4"),
+        UIImage(named: "picture5"),
+        UIImage(named: "picture1"),
+        UIImage(named: "picture2"),
+        UIImage(named: "picture3"),
+        UIImage(named: "picture4"),
+        UIImage(named: "picture5"),
+        UIImage(named: "picture1"),
+        UIImage(named: "picture5"),
+        UIImage(named: "picture1"),
+        UIImage(named: "picture2"),
+        UIImage(named: "picture3"),
+        UIImage(named: "picture4"),
+        UIImage(named: "picture5"),
+        UIImage(named: "picture1"),
+        UIImage(named: "picture1"),
+        UIImage(named: "picture1"),
+        UIImage(named: "picture2"),
+        UIImage(named: "picture3"),
+        UIImage(named: "picture4"),
+        UIImage(named: "picture5"),
+        UIImage(named: "picture1"),
+        UIImage(named: "picture2"),
+        UIImage(named: "picture3"),
+        UIImage(named: "picture4"),
+        UIImage(named: "picture5"),
+        UIImage(named: "picture1"),
+        UIImage(named: "picture1"),
+        UIImage(named: "picture2"),
+        UIImage(named: "picture3"),
+        UIImage(named: "picture4"),
+        UIImage(named: "picture5"),
+        UIImage(named: "picture1"),
+        UIImage(named: "picture2"),
+        UIImage(named: "picture3"),
+        UIImage(named: "picture4"),
+        UIImage(named: "picture5"),
+        UIImage(named: "picture1"),
+        UIImage(named: "picture5"),
+        UIImage(named: "picture1"),
+        UIImage(named: "picture2"),
+        UIImage(named: "picture3"),
+        UIImage(named: "picture4"),
+        UIImage(named: "picture5"),
+        UIImage(named: "picture1"),
+        UIImage(named: "picture1"),
+        UIImage(named: "picture3"),
+        UIImage(named: "picture4"),
+        UIImage(named: "picture5"),
+        UIImage(named: "picture1"),
+        UIImage(named: "picture1"),
+        UIImage(named: "picture2"),
+        UIImage(named: "picture3"),
+        UIImage(named: "picture4"),
+        UIImage(named: "picture5"),
+        UIImage(named: "picture1"),
+        UIImage(named: "picture2"),
+        UIImage(named: "picture3"),
+        UIImage(named: "picture4"),
+        UIImage(named: "picture5"),
+        UIImage(named: "picture1"),
+        UIImage(named: "picture5"),
+        UIImage(named: "picture1"),
+        UIImage(named: "picture2"),
+        UIImage(named: "picture3"),
+        UIImage(named: "picture4"),
+        UIImage(named: "picture5"),
+        UIImage(named: "picture1"),
+        UIImage(named: "picture1")
+    ]
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        
+        collectionView.register(UINib(nibName: "ImageCell", bundle: nil), forCellWithReuseIdentifier: "ImageReuseableCell")
+    }
+
+
+}
+
+extension ViewController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return images.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ImageReuseableCell", for: indexPath) as! ImageCell
+        
+        cell.imageView.image = images[indexPath.row]
+        
+        return cell
+        
+    }
+}
+
+extension ViewController: UICollectionViewDelegateFlowLayout {
+    
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+//        let itemPerRow: CGFloat = UIDevice.current.userInterfaceIdiom == .pad ? 6 : 4
+//        let padding: CGFloat = 10
+//        let totalPadding = padding * (itemPerRow - 1)
+//        let availableWidth = collectionView.frame.width - totalPadding
+//        let itemWidth = availableWidth / itemPerRow
+//        
+//        return CGSize(width: itemWidth, height: itemWidth)
+//    }
+    
+}
---
+++ CollectionViewTask/View/Image Cell/ImageCell.swift
@@ -0,0 +1,19 @@
+//
+//  ImageCell.swift
+//  CollectionViewTask
+//
+//  Created by Muhammad Hasnain Bangash on 23/11/2024.
+//
+
+import UIKit
+
+class ImageCell: UICollectionViewCell {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
